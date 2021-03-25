942f4f57910fa006dbaa049fc46b22ba1db80da1
Michael Scott <michael.scott@drake.edu>
2021-03-24 21:55:53 -0500

    Display results of csv file

--- CSCapstone-main/CSCapstone-main/FlaskApp/flaskapp.py
+++ CSCapstone-main/CSCapstone-main/FlaskApp/flaskapp.py
@@ -1,46 +1,43 @@
 from flask import Flask
 from flask import render_template, request, redirect
 import os
+import tablib
+
 app = Flask(__name__)
 
 
-@app.route('/DCASE', methods=["GET", "POST"])
+@app.route("/DCASE", methods=["GET", "POST"])
 def DCASE():
 
-	# if request.method == "POST":
-	# 	if request.files:
-	# 		file_one = request.files["sound"]
-	# 		file_one.save(os.path.join(app.config["IMAGE UPLOADS"], file_one.filename)) 
-	# 		print("it worked")
-	# 		return redirect(request.url)
-
-	return render_template('DCASE.html')
+    return render_template("DCASE.html")
 
 
-app.config["IMAGE UPLOADS"] = "/Users/michaelscott/desktop/capstone-project-team-6-sound-cruncher/CSCapstone-main/cscapstone-main/flaskapp/images"
+app.config["IMAGE UPLOADS"] = "__your_path_here__/flaskapp/SoundSample"
 
 
-@app.route('/results', methods=["GET", "POST"])
+@app.route("/loading", methods=["GET", "POST"])
 def results():
-	
-	if request.method == "POST":
-		if request.files:
-			file_one = request.files["sound"]
-			file_one.save(os.path.join(app.config["IMAGE UPLOADS"], file_one.filename)) 
-			print("it worked")
-			return redirect(request.url)
-	return 'Results'
+    result = "bird"
 
+    if request.method == "POST":
+        if request.files:
+            file_one = request.files["sound"]
+            file_one.save(os.path.join(app.config["IMAGE UPLOADS"], file_one.filename))
+            print("it worked")
+            return redirect(request.url)
+    return render_template("loading.html")
 
-from flask import render_template
-
-@app.route("/hello/<username>/")
-def hello_user(username):
-	return render_template('layout.html', name=username)
-
-	
+#User snippsat on python-forum.io
+#https://python-forum.io/Thread-show-csv-file-in-flask-template-html
+dataset = tablib.Dataset()
+with open(os.path.join(os.path.dirname(__file__),'result.csv')) as f:
+    dataset.csv = f.read()
+ 
+@app.route("/results")
+def index():    
+    return dataset.html
 
 
 # these two lines of code should always be the last in the file
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
