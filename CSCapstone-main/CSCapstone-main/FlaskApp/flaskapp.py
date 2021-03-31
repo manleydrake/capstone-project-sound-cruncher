abc5f5fae936f4af35da0e91ac43f3d5c93cc07e
Michael-S-cott <michael.scott@drake.edu>
2021-03-31 10:43:07 -0500

    Test

--- CSCapstone-main/CSCapstone-main/FlaskApp/flaskapp.py
+++ CSCapstone-main/CSCapstone-main/FlaskApp/flaskapp.py
@@ -12,23 +12,26 @@ def DCASE():
     return render_template("DCASE.html")
 
 
-app.config["IMAGE UPLOADS"] = "__your_path_here__/flaskapp/SoundSample"
+app.config["IMAGE UPLOADS"] = "/Users/michael/desktop/cscapstone-main/flaskapp/soundsample"
 
 
 @app.route("/loading", methods=["GET", "POST"])
 def results():
-    result = "bird"
-
     if request.method == "POST":
+        print("run prediction files here")
+        os.system("./write.sh")
         if request.files:
             file_one = request.files["sound"]
             file_one.save(os.path.join(app.config["IMAGE UPLOADS"], file_one.filename))
-            print("it worked")
             return redirect(request.url)
     return render_template("loading.html")
 
 #User snippsat on python-forum.io
 #https://python-forum.io/Thread-show-csv-file-in-flask-template-html
+
+#pip install tablib
+#pip install "tablib[html]" 
+
 dataset = tablib.Dataset()
 with open(os.path.join(os.path.dirname(__file__),'result.csv')) as f:
     dataset.csv = f.read()
