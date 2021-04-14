45070579a5f738eaa60b910e036ef12843021e65
briandannenmueller <49847805+briandannenmueller@users.noreply.github.com>
2021-04-14 10:44:01 -0500

    added species_select.py

---
+++ MLProgram/preprocess/species_select.py
@@ -0,0 +1,16 @@
+
+
+
+# define a function that will search the xml for MediaID, 
+# Genus, and Species and return them as a tuple "s_tuple"
+
+# define a function that compares that the genus and species against the
+# list of selected classes and returns True if there is a match
+
+
+# Loop through the xml directory and grab all xmls that have one of the 
+# selected genus and species. Place in separate directory. 
+# The MediaID, Genus, and Species should also be stored in a dictionary, "s_dict". 
+
+# Loop through the wav directory and grab all wav files with the 
+# MediaID's contained in the dictionary. Place in a separate dictionary 
