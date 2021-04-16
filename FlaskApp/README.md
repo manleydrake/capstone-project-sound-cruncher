47d7df3a361dba7a3ced5651437e8615dab7691e
Michael-S-cott <michael.scott@drake.edu>
2021-04-16 16:53:33 -0500

    Manually display csv results
    Removed tablib, now just manually display the results of the csv file in an html table

--- FlaskApp/README.md
+++ FlaskApp/README.md
@@ -1,11 +1,9 @@
 # CSCapstone
 
- 3-24
-
 !!add path in line 15!!
 flaskapp.py is executable file
 
-/DCASE prompts user to submit a file. Does not check if it is a sound file yet. File saved to SoundSample folder
+/ prompts user to submit a file. Does not check if it is a sound file yet. File saved to SoundSample folder
 
 /loading is when I think we will generate our guess
 
--- FlaskApp/flaskapp.py
+++ FlaskApp/flaskapp.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask import render_template, request, redirect
 import os
-import tablib
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def DCASE():
-
     return render_template("DCASE.html")
 
 
@@ -16,7 +14,7 @@ app.config["IMAGE UPLOADS"] = "/Users/michael/desktop/cscapstone-main/flaskapp/s
 
 
 @app.route("/loading", methods=["GET", "POST"])
-def results():
+def loading():
     if request.method == "POST":
         print("run prediction files here")
         os.system("./write.sh")
@@ -25,20 +23,26 @@ def results():
             file_one.save(os.path.join(app.config["IMAGE UPLOADS"], file_one.filename))
             return redirect(request.url)
     return render_template("loading.html")
-
-#User snippsat on python-forum.io
-#https://python-forum.io/Thread-show-csv-file-in-flask-template-html
-
-#pip install tablib
-#pip install "tablib[html]" 
-
-dataset = tablib.Dataset()
-with open(os.path.join(os.path.dirname(__file__),'result.csv')) as f:
-    dataset.csv = f.read()
  
+
 @app.route("/results")
-def index():    
-    return dataset.html
+def results(): 
+    with open('result.csv', encoding = 'ISO-8859-1') as f:
+        f1 = f.readlines()
+        key = []
+        value = []
+        html_str = '<style> table, th, td{ border :1px solid black; align-content:center; text-align:center; } body { background-color: rgb(0, 204, 153); outline: black; }caption{ font-size:24; } </style> <table style="width:100%"> <caption>Prediction</caption> <tr> <th>'
+        for line in f1:
+            if 'Column' in line:
+                key = (line.split(','))
+
+            else:
+                value = line.split(',')
+
+        html_str = html_str + key[0] + '</th><th>' + key[1] + '</th> <th>' + key[2] + '</th> </tr>'
+        html_str = html_str + ' <tr> <td>' + value[0] + '</td> <td>' + value[1] + '</td> <td>' + value[2] + '</td> </tr> </table>'
+
+    return html_str
 
 
 # these two lines of code should always be the last in the file
