8b9e3b204e8d3f0033830962f8d4471da34434d3
briandannenmueller <49847805+briandannenmueller@users.noreply.github.com>
2021-04-25 22:08:24 -0500

    completed species_select.py

--- MLProgram/preprocess/species_select.py
+++ MLProgram/preprocess/species_select.py
@@ -1,20 +1,5 @@
 
-
-
-# define a function that will search the xml for MediaID, 
-# Genus, and Species and return them as a tuple "s_tuple"
-
-
-# define a function that compares that the genus and species against the
-# list of selected classes and returns True if there is a match
-
-
-# Loop through the xml directory and grab all xmls that have one of the 
-# selected genus and species. Place in separate directory. 
-# The MediaID, Genus, and Species should also be stored in a dictionary, "s_dict". 
-
-# Loop through the wav directory and grab all wav files with the 
-# MediaID's contained in the dictionary. Place in a separate dictionary 
+#place this file in the test or Training Set folder and run it from command line from within that directory to subset data based on your chosen classes. 
 
 import csv
 import os
@@ -34,36 +19,48 @@ def mk_species_dict(f):
 			line_count += 1
 	return species_dict 
 
+def copy_files(species_dict):
+	
+	n_species = len(species_dict)
+	#create main folder and subfolders for storing subset
+	main = str(n_species) + "_" + "Species"
+	os.mkdir(main)
+	os.mkdir(main + "/xml")
+	os.mkdir(main + "/wav")
 
-n_species = len(species_dict)
-#Creates a directory for each species
-for index in range(n_species):
-	os.mkdir(list[index])
+	#Loops through each file in the directory 
+	for filename in os.listdir('xml'):
+		try:
+			f = open("xml/" + filename, 'r', encoding='ISO-8859-1')
+			f1 = f.readlines()
+			for line in f1:
+				#Copies the corresponding wav file(s) to folder
+				for key, value in species_dict.items():
+					#check if xml indicates the desired Species
+					if ('<Species>' + value[1] + '</Species>' in line) and ('<Genus>' + value[0] + '</Genus>' in prev_line):
+						#path of original xml file
+						original = "xml/" + filename
+						#target path for xml
+						target = main + "/" + original
+						#copy wav and xml to new folder
+						shutil.copyfile(original, target)
+						print("xml copy successful for " + key)
 
-#Loops through each file in the directory 
-for filename in os.listdir('xml'):
-	#os.system("echo " + filename)
-	f = open(filename, 'r', encoding='ISO-8859-1')
-	f1 = f.readlines()
-	for line in f1:
-		#Copies the corresponding wav file(s) to folder
-		for key, value in species_dict:
-			#change Vernacular Names to Genus and Species
-			if ('<Genus>' + value[0] + '</Genus>' in line) and ('<Species>' + value[1] + '</Species>' in line) :#sorting by vernacular name
-				file_found = True
-				filename2 = filename[:-3]
-				filename2 = filename2 + 'wav'#Gets corresponding wav file
-				original = r'/wav/' + filename2
-				target = r'' + list[value[0]] + list(value[1]) + '/' + filename2
-				shutil.copyfile(original, target)#copy file
+						#path of original wav file
+						original = "wav/" + filename[:-3] + "wav"
+						#target path for wav copy
+						target = main + "/" + original
+						shutil.copyfile(original, target)
+						print("wav copy successful for " + key)
+				prev_line = line	
+			f.close()
+		except FileNotFoundError as error:
+			print(error)
+		
 
+species = mk_species_dict("species.csv")
+copy_files(species)
 
-	f.close()
-
-# TODO:
-# Make sure selected XML's and WAV's are placed into their own directories (make these directories)
-# Rearrange so that outer loop is iterating on species dict, rather than other way around
-# Modularize. 
 
 
 
